867f8217295410f1467cdb58c9bb170c3167d27b
Ujwala2Uk <roopaumesha2@gmail.com>
2022-10-18 17:20:46 +0530

    Add project files.

---
+++ Controllers/KanopiPagesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Kanopi_page.Models;
+
+namespace Kanopi_page.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KanopiPagesController : ControllerBase
+    {
+        private readonly Kanopi_appContext _context;
+
+        public KanopiPagesController(Kanopi_appContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/KanopiPages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KanopiPage>>> GetKanopiPages()
+        {
+            return await _context.KanopiPages.ToListAsync();
+        }
+
+        // GET: api/KanopiPages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KanopiPage>> GetKanopiPage(int id)
+        {
+            var kanopiPage = await _context.KanopiPages.FindAsync(id);
+
+            if (kanopiPage == null)
+            {
+                return NotFound();
+            }
+
+            return kanopiPage;
+        }
+
+        // PUT: api/KanopiPages/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKanopiPage(int id, KanopiPage kanopiPage)
+        {
+            if (id != kanopiPage.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(kanopiPage).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!KanopiPageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/KanopiPages
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<KanopiPage>> PostKanopiPage(KanopiPage kanopiPage)
+        {
+            _context.KanopiPages.Add(kanopiPage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetKanopiPage", new { id = kanopiPage.Id }, kanopiPage);
+        }
+
+        // DELETE: api/KanopiPages/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKanopiPage(int id)
+        {
+            var kanopiPage = await _context.KanopiPages.FindAsync(id);
+            if (kanopiPage == null)
+            {
+                return NotFound();
+            }
+
+            _context.KanopiPages.Remove(kanopiPage);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool KanopiPageExists(int id)
+        {
+            return _context.KanopiPages.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Kanopi_appContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Kanopi_page.Models
+{
+    public partial class Kanopi_appContext : DbContext
+    {
+        public Kanopi_appContext()
+        {
+        }
+
+        public Kanopi_appContext(DbContextOptions<Kanopi_appContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<KanopiPage> KanopiPages { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=ELW5143\\SQLEXPRESS;Database=Kanopi_app;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<KanopiPage>(entity =>
+            {
+                entity.ToTable("kanopi_page");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
